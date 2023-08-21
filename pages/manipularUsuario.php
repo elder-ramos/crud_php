50236667a4a86332eb555c5d47d008da4d9b8220
elder-ramos <99875876+elder-ramos@users.noreply.github.com>
2023-08-21 12:48:01 -0300

    Add API para obter última atualização

--- pages/manipularUsuario.php
+++ pages/manipularUsuario.php
@@ -4,18 +4,32 @@
             $tel = $_POST["telefone"];
             $codigo = $_POST["encomenda"];
 
-            $sql = "INSERT INTO usuarios (numero, codigo) VALUES ('{$tel}', '{$codigo}')";
+            $api = file_get_contents("https://api.linketrack.com/track/json?user=elderrramos@gmail.com&token=2b0f5d48bb39f9700990173e1f9fb6c1a1ae57491e87b09e25c7d07c1d656547&codigo=".$codigo);
 
-            $res = $conexao->query($sql);
-
-            if($res==true){
-                print"<script>alert('Cadastro realizado com sucesso!')</script>";
+            // código inválido
+            if($api == FALSE){
+                print "<script>alert('ERRO! Código inválido')</script>";
                 print"<meta http-equiv='refresh' content='0; url=index.php?page=listar'>";
+                break;
             }else{
-                print"<script>alert('ERRO! Cadastro NÃO realizado')</script>";
-                print"<meta http-equiv='refresh' content='0; url=index.php?page=listar'>";
+                $objetoAPI = json_decode($api);
+                $data = $objetoAPI->eventos[0]->data;
+
+                // dd/mm/yyyy -> yyyy/mm/dd + hh:mm
+                $dateTime = implode('-', array_reverse(explode('/', "{$data}")))." ".$objetoAPI->eventos[0]->hora;
+
+                $sql = "INSERT INTO usuarios (numero, codigo, atualizacao) VALUES ('{$tel}', '{$codigo}', '{$dateTime}')";
+                $res = $conexao->query($sql);
+
+                if($res==true){
+                    print"<script>alert('Cadastro realizado com sucesso!')</script>";
+                    print"<meta http-equiv='refresh' content='0; url=index.php?page=listar'>";
+                }else{
+                    print"<script>alert('ERRO! Cadastro NÃO realizado')</script>";
+                    print"<meta http-equiv='refresh' content='0; url=index.php?page=listar'>";
+                }
+                break;
             }
-            break;
         case "editar":
             $tel = $_POST["telefone"];
             $codigo = $_POST["encomenda"];
